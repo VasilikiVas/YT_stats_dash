e6a04687722a11a5ba4cb34706011bd1e66fb230
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2022-06-23 11:14:41 +0200

    Added overall structure of the HTML

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, jsonify, send_from_directory
-import os, json
+import os, json, sys
 
 from decimal import Decimal
 
@@ -8,7 +8,22 @@ import numpy as np
 
 from . import main
 
+DATA_DIR = os.path.join("data")
 
 @main.route('/', methods=['GET'])
-def index():
-	return render_template("index.html")
+@main.route('/category', methods=['GET'])
+def category():
+	cat = request.args.get("category")
+	title_view = request.args.get("title_view")
+
+	if not cat:
+		cat = "gaming"
+
+	channels_info_path = os.path.join(DATA_DIR, f"channels-info_{cat}.json")
+	with open(channels_info_path, "r") as f:
+		channels_dict = json.load(f)
+	channels = [{"name": name, **info} for name,info in channels_dict.items()]
+
+	return render_template("category.html", channels=channels[:5], category={"name": cat, "subs": 190000})
+
+
