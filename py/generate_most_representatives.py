0495fe7170505799092ee9fdf388250b8639e6ae
ObviouslyLuuk <54840183+ObviouslyLuuk@users.noreply.github.com>
2022-06-24 10:27:37 +0200

    fixed terminology creator->channel; get_creator_stats.py filename still needs to be changed but that would cause issues with git if someone else also made changes;

--- py/generate_most_representatives.py
+++ py/generate_most_representatives.py
@@ -5,11 +5,13 @@ Project: YouTube statistics visualization
 License for 3rd party use: Anyone can use this lol I don't mind
 """
 
-from get_creator_stats import *
+from get_channel_stats import *
 import numpy as np
 import torch
 import json
 import os
+
+from util.constants import Topic
 
 
 def generate_most_repr(data_type):
@@ -21,8 +23,7 @@ def generate_most_repr(data_type):
         - data_type: the type of representation we're working on. Can be 'thumbnail' or 'title'
     """
 
-    categories = ["animals", "autos", "blogs", "comedy", "education", "entertainment"\
-                    "gaming", "howto", "science"]
+    categories = Topic._member_names_
     
     out_dir = os.path.join(DATA_DIR, f"{data_type}_representatives")
     if not os.path.isdir(out_dir):
@@ -32,25 +33,25 @@ def generate_most_repr(data_type):
         most_repr_dic = {}
 
         with open(os.path.join("..", "data", f"videos-info_{cat}.json"), "r") as f:
-            creator_info = json.load(f)
-            creator_names = creator_info.keys()
+            channel_info = json.load(f)
+            channel_names = channel_info.keys()
 
         # Already calculate the mean (yes I know this is an extra for loop, but else you have to
-        # store all latent representations of all creators in memory)
-        category_avg = torch.mean([get_mean_latent(cat, creator, data_type) for creator in creator_names],dim=0)
+        # store all latent representations of all channels in memory)
+        category_avg = torch.mean([get_mean_latent(cat, channel, data_type) for channel in channel_names],dim=0)
         all_vids = {}
 
-        for creator in creator_names:
-            all_creator_latents = get_all_latents(cat, creator, data_type)
-            creator_avg = get_mean_latent(cat, creator, data_type)
+        for channel in channel_names:
+            all_channel_latents = get_all_latents(cat, channel, data_type)
+            channel_avg = get_mean_latent(cat, channel, data_type)
             similarities = []
 
-            for vid_id, latent in all_creator_latents.items():
-                similarities.append(abs(np.dot(latent, creator_avg)/np.linalg.norm(latent)*np.linalg.norm(creator_avg)))
+            for vid_id, latent in all_channel_latents.items():
+                similarities.append(abs(np.dot(latent, channel_avg)/np.linalg.norm(latent)*np.linalg.norm(channel_avg)))
                 all_vids[vid_id] = abs(np.dot(latent, category_avg)/np.linalg.norm(latent)*np.linalg.norm(category_avg))
 
-            most_similar_vid_id = all_creator_latents.keys()[np.argmin(similarities)]
-            most_repr_dic[creator] = most_similar_vid_id
+            most_similar_vid_id = all_channel_latents.keys()[np.argmin(similarities)]
+            most_repr_dic[channel] = most_similar_vid_id
 
         cat_most_similar_vid_id = all_vids.keys()[np.argmin(all_vids.values())]
         most_repr_dic[f"Category_{cat}"] = cat_most_similar_vid_id
