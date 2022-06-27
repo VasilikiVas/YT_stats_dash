a016ae700db688202b7552696178264d5f0cf70b
Cyproxius <maris@vrijheid.net>
2022-06-27 12:14:27 +0200

    Added load more button to thumbnail display, but messes up CSS

--- app/static/js/display_category.js
+++ app/static/js/display_category.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', (event) => {
     displayChannels();
+    // displayVideos();
 })
 
-var num_channels_display = 5;
+var num_channels_display = 10;
+var num_videos_display = 10
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
@@ -11,7 +13,7 @@ function removeAllChildNodes(parent) {
 }
 
 function increaseDisplayedChannels(){
-    num_channels_display += 5;
+    num_channels_display += 10;
     displayChannels();
 };
 
@@ -33,6 +35,7 @@ function displayChannels(){
         var channel_link = document.createElement("a");
         channel_link.className = "nav-link";
         channel_link.setAttribute("href", `/channel/${channel.name}`);
+        channel_link.style.cssText = "width: 100% !important";
 
         var channel_name = document.createElement("span")
         channel_name.innerHTML = `${channel.name}`
@@ -50,9 +53,48 @@ function displayChannels(){
     };
     var load_more_button = document.createElement("button");
     load_more_button.innerText = "Load more...";
-    load_more_button.setAttribute("id", "load_more_button");
+    load_more_button.setAttribute("id", "load_more_channels_button");
     load_more_button.setAttribute("onclick", "increaseDisplayedChannels()");
+
     channelsDisplay.appendChild(load_more_button);    
 };
 
-// document.getElementById("load_more_button").onclick = increaseDisplayedChannels;
+
+function increaseDisplayedVideos(){
+    num_videos_display += 10;
+    displayVideos();
+};
+
+function displayVideos(){
+
+    var videosDisplay = document.getElementById("video_display_list");
+
+    // Remove all children so as to not have duplicate channels displayed
+    removeAllChildNodes(videosDisplay)
+
+    // Get the amount of channels to display
+    videos_to_display = videos.slice(0,num_videos_display);
+    for (let i=0; i<videos_to_display.length;i++){
+        // <a href="/video/{{ vid.id }}"><img src="https://i.ytimg.com/vi/{{ vid.id }}/default.jpg" class="video_list_thumb"></a>
+        var vid = videos[i];
+
+        var video_link = document.createElement("a");
+        video_link.setAttribute("href", `/video/${vid.id}`);
+        video_link.style.cssText = "";
+
+        var video_img = document.createElement("img")
+        video_img.className = "video_list_thumb";
+        video_img.setAttribute("src", `https://i.ytimg.com/vi/${vid.id}/default.jpg`);
+        video_img.style.cssText = "";
+
+        videosDisplay.appendChild(video_img);
+        videosDisplay.appendChild(video_link);
+
+    };
+    var load_more_button = document.createElement("button");
+    load_more_button.innerText = "Load more...";
+    load_more_button.setAttribute("id", "load_more_vids_button");
+    load_more_button.setAttribute("onclick", "increaseDisplayedVideos()");
+
+    videosDisplay.appendChild(load_more_button);    
+};
