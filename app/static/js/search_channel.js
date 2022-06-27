c14fed312335c7cfb4182754c980ef949fedaa4b
Cyproxius <maris@vrijheid.net>
2022-06-27 15:23:14 +0200

    redundant code removed

--- app/static/js/search_channel.js
+++ app/static/js/search_channel.js
@@ -9,11 +9,7 @@ function search_channels(){
         var channel = channels[i];
         if (channel.name.substring(0, input_str.length) == input_str){
             selected_channels.push(channel);
-            // display_counter++;
         }
-        // if (display_counter >= num_videos_display){
-        //     break;
-        // }
     };
     // Display the first selected N channels 
     var search_channels_to_display = selected_channels.slice(0,num_channels_display);
